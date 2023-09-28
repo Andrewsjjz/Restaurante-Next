32405cc5840e408deb609957b5d83e9ecd2f58d8
Andres Jimenez <126204033+Andrewsjjz@users.noreply.github.com>
2023-09-28 10:46:59 -0400

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,21 +8,25 @@ const Sidebar = () => {
   return (
     <>
     <div className="flex justify-center items-center xl:justify-center xl:items-center 2xl:justify-center 2xl:items-center">
+    
       <Image
       height={200}
       width={200}
       src="/assest/LogoRestaurante.jpg"
       alt="Logo tipo"
       />
+        
     </div>
 
       <nav className="mt-10">
+        
       {categorias.map( categoria =>(
         <NavCategoria
         key={categoria.id}
         categoria={categoria}
         />
       ))}
+          
       </nav>
     </>
   )
