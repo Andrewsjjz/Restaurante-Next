e3cf1b04d88b2159b58704c729e3faf33c88079d
Andres Jimenez <126204033+Andrewsjjz@users.noreply.github.com>
2023-10-02 08:47:40 -0400

    Update [id].js

--- pages/api/ordenes/[id].js
+++ pages/api/ordenes/[id].js
@@ -10,13 +10,18 @@ export default async function handler(req, res) {
         const {id } = req.query
 
         const ordenActualizada = await prisma.orden.update({
+            
             where: {
                 id: parseInt(id)
             }, 
+            
             data:{
+                
                 estado: true
+                
             }
         })
+        
         res.status(200).json(ordenActualizada)
     }
 
