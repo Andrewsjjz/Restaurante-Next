93a09f773cf0fbd494ff30ec2bcf4098824ff064
Andres Jimenez <126204033+Andrewsjjz@users.noreply.github.com>
2023-10-02 08:48:38 -0400

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -17,11 +17,16 @@ export default function Admin() {
     <p className="font-black text-3xl mt-10 mb-10 text-center">Chequea y administra los pedidos</p>
 
     {data && data.length ? data.map( orden => 
+        
             <OrdenesAdmin
             key={orden.id}
             orden={orden}
             />
-    ) : <h1>No hay ordenes pendientes</h1>}
+                
+    ) : 
+        <h1>No hay ordenes pendientes</h1>
+                }
+
     </AdminLayout>
     </>
   )
