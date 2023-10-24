49bd92246fcaebdc920d6afb2a99a81673042a4e
Andres Jimenez <126204033+Andrewsjjz@users.noreply.github.com>
2023-10-23 20:38:57 -0400

    Update OrdenesAdmin.js

--- components/OrdenesAdmin.js
+++ components/OrdenesAdmin.js
@@ -13,14 +13,11 @@ export default function OrdenesAdmin({ orden }) {
         if(confirm('¿Seguro que quiere completar la orden?')){
             try {
                 await axios.post(`/api/ordenes/${id}`)
-    
                 toast.success('Orden completada')
-    
            } catch (error) {
             toast.error('Ups, ocurrio un problema')
            }
         }
-
     }
 
 
@@ -29,8 +26,7 @@ export default function OrdenesAdmin({ orden }) {
         <div className="p-10 space-y-5 mt-5 border-4">
 
             <div className="md:flex md:items-start md:justify-between">
-            <p className="font-normal text-2xl">Nombre del cliente: <span className="font-bold">{cliente}</span>
-            </p>
+            <p className="font-normal text-2xl">Nombre del cliente: <span className="font-bold">{cliente}</span></p>
         
             <button 
             onClick={() => {
@@ -45,6 +41,7 @@ export default function OrdenesAdmin({ orden }) {
             <p className="font-normal text-2xl">Id de la orden: <span className="font-bold">{id}</span></p>
 
             <div>
+                
                 {pedido.map(platillos =>
                     <div key={platillos.id} platillos={platillos} className=" py-3 flex items-center last-of-type:border-0">
                         <div className="w-32">
@@ -63,8 +60,10 @@ export default function OrdenesAdmin({ orden }) {
                 )}
                     
                 <div className="flex justify-end items-end">
+                    
                     <h1 className="text-3xl uppercase">Total a pagar: {''}
                         <span className="text-3xl font-bold uppercase">${formatearDinero(total)}</span></h1>
+                            
                 </div>
 
             </div>
